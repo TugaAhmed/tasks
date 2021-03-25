822851960f155f0a664cc4d1d2c885ee4cf62328
TugaAhmed <tugakariem@gmail.com>
2021-03-25 09:57:25 +0200

    .

--- tasks/palette_tracking/model/models.py
+++ tasks/palette_tracking/model/models.py
@@ -6,16 +6,17 @@ class palette_tracking(models.Model):
 
 
     picking_id = fields.Many2one('stock.picking' , string='Picking',required=True)
-    partner_id = fields.Many2one('res.partner', string='Partner' , default=lambda self: self.env.user.partner_id)
+    partner_id = fields.Many2one('res.partner', string='Partner' )
     license_plate = fields.Char('License Plate')
-    picking_partner_id = fields.Many2one('res.partner',string='Picking Partner')
-    picking_date_done = fields.Datetime('Picking Date Done')
+    picking_partner_id = fields.Many2one('res.partner',string='Picking Partner',related='picking_id.partner_id')
+    picking_date_done = fields.Datetime('Picking Date Done',related='picking_id.date_done')
     pallet_count_plus = fields.Integer('Pallet Count Plus')
     pallet_count_minus = fields.Integer('Pallet Count Minus')
     balance = fields.Integer(string='Balance' , compute="get_balance")
 
 
+    @api.depends('pallet_count_plus','pallet_count_minus')
     def get_balance(self):
     	self.balance = 0.0
     	for record in self :
-    		record.balance = record.pallet_count_plus - pallet_count_minus
+    		record.balance = record.pallet_count_plus - record.pallet_count_minus
