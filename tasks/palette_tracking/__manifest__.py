244a1e9a0902281d08c7fc6a1d9dc09af02af6c6
Tuga Yousif <tugakariem@gmail.com>
2021-03-25 09:06:31 +0200

    Add files via upload

---
+++ tasks/palette_tracking/__manifest__.py
@@ -0,0 +1,26 @@
+{
+
+
+    'name': 'Palette Tracking',
+    'category': 'stock',
+    'summary': 'Palette Tracking',
+    'version': '0.1',
+    	
+    'depends': [
+       'base', 'sale' , 'stock','sale_stock','sale_management'
+    ],
+
+    'data': [
+    
+       'views/views.xml',
+      
+
+
+
+		
+    ],
+
+    # Technical
+    'installable': True,
+    
+}
